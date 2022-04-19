500c7209ff90bfc718ec5c2188dde65026d6709c
Fake-GitHub-Admin <81976991+Fake-GitHub-Admin@users.noreply.github.com>
2022-04-19 17:07:00 -0400

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -12,7 +12,11 @@ namespace MyApplication
       Console.WriteLine(x);  
       int x = 5;
       int y = 3;
-      Console.WriteLine(x + y);  
+      Console.WriteLine(x + y); 
+      Console.WriteLine(Math.Max(5, 10)); 
+      Console.WriteLine(Math.Sqrt(64));
+      Console.WriteLine(Math.Abs(-4.7));
+      Console.WriteLine(Math.Round(9.99));
      
      }
   }
