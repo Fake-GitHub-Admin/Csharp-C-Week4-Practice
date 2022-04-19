4d6b473303b1dfa1919f3b67f6e97333a5baa0e4
Fake-GitHub-Admin <81976991+Fake-GitHub-Admin@users.noreply.github.com>
2022-04-19 16:28:30 -0400

    Create practice.cs

---
+++ practice.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+namespace MyApplication
+{
+  class Practice
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      x++;
+      Console.WriteLine(x);  
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x + y);  
+     
+     }
+  }
+}
